891a0cb5bffaf7866dc8565a68b1160c3848086f
unknown <roopeshb13@gmail.com>
2022-07-11 15:13:15 +0530

    updated csv script

--- scripts/csvScript.js
+++ scripts/csvScript.js
@@ -4,7 +4,6 @@ const excelToJson = require('convert-excel-to-json');
 const result = excelToJson({
     sourceFile: "C:/Users/Roopesh/Desktop/Book1.xlsx" // fs.readFileSync return a Buffer
 });
-//First sheet should be named "Book1"
 
 var jsonData = JSON.stringify(result)
 
@@ -13,22 +12,23 @@ fs.writeFile("../public/data.json", jsonData, function (err) {
         console.log("An error occured while writing JSON Object to File.");
         return console.log(err);
     }
-    console.log("JSON file has been saved.");
+    const json = require('../public/data.json')
+    let final_data = []
+    var sheet_Name
+    for (let key in json) {
+        sheet_Name = key
+        break
+    }
+    for (let i = 1; i < json[sheet_Name].length; i++) {
+
+        final_data.push({ "word": json[sheet_Name][i].A, "weight": json[sheet_Name][i].B, "click": [{ 'label': 'business', 'link': 'https://www.google.com' }, { 'label': "finance", 'link': 'https://www.yahoo.com' }, { 'label': 'account', 'link': 'https://www.duckduckgo.com' },] })
+    }
+    fs.writeFile("../public/data.json", JSON.stringify(final_data), function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    });
 })
 
-//first run code above with below code commented and then run the code below
-
-
-const json = require('../public/data.json')
-let final_data = []
-for (let i = 1; i < json.Book1.length; i++) {
-
-    final_data.push({ "word": json.Book1[i].A, "weight": json.Book1[i].B, "click": [{ 'label': 'business', 'link': 'https://www.google.com' }, { 'label': "finance", 'link': 'https://www.yahoo.com' }, { 'label': 'account', 'link': 'https://www.duckduckgo.com' },] })
-}
-fs.writeFile("../public/data.json", JSON.stringify(final_data), function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
-    console.log("JSON file has been saved.");
-});
