812d773452255607c2498d6b783b1304d49a01c8
unknown <roopeshb13@gmail.com>
2022-07-14 14:54:19 +0530

    thumbnail and popup wordcloud display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Link, Button } from "@mui/material";
-
+import { Link, Box } from "@mui/material";
 try {
   const Wordcloud = require("wordcloud");
 } catch (err) {
@@ -8,25 +7,28 @@ try {
 }
 // for deployment use above code.
 
-export default function Cloud({ prop }) {
+export default function Cloud() {
   var canvasRef = useRef(null);
+  var thumbnailCanvasRef = useRef(null);
   const Wordcloud = require("wordcloud");
 
   const queryParams = new URLSearchParams(window.location.search);
   const filename = queryParams.get("input") || "words";
   const data = require("../public/" + filename + ".json").words;
 
-
   const [pop, setPop] = useState(false);
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(data.length);
   const [word, setWord] = useState("");
   const [props, setProps] = useState([]);
   const [max, setMax] = useState(0);
-  const [count, setCount] = useState(data.length);
+
   const [maxWeight, setMaxWeight] = useState(0);
+  const [open, setOpen] = useState(false);
   var timer = null
   const canvasHeight = 500;
-  const canvasWidth = 1500;
+  const canvasWidth = 800;
+  const thumbnailCanvasHeight = 50;
+  const thumbnailCanvasWidth = 100;
   //edit canvasWidth to make the cloud bigger/smaller
 
   const styles = {
@@ -36,6 +38,19 @@ export default function Cloud({ prop }) {
     linkColor: "red",
     caption: "This is a Programming Language cloud",
     hoverTimeout: "5000" //timeout in ms for popup
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: canvasWidth,
+    height: canvasHeight + 100,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
   };
 
   function popup(item, event) {
@@ -64,10 +79,22 @@ export default function Cloud({ prop }) {
     }
   }
 
+  // Overwrite Math.random to use seed to ensure same word cloud is printed on every render
+  function randseed(s) {
+    s = Math.sin(s) * 10000;
+    return s - Math.floor(s);
+  }
+
+  let seed = 0;
+  Math.random = function () {
+    seed++;
+    return randseed(seed);
+  };
+
   const handleSubmit = () => {
     try {
       if (number < data.length) {
-        setCount(number);
+        // setCount(number);
         setMaxWeight(0);
       } else {
         //
@@ -87,20 +114,10 @@ export default function Cloud({ prop }) {
     return res;
   }
 
-  // Overwrite Math.random to use seed to ensure same word cloud is printed on every render
-  function randseed(s) {
-    s = Math.sin(s) * 10000;
-    return s - Math.floor(s);
-  }
-  let seed = 0;
-  Math.random = function () {
-    seed++;
-    return randseed(seed);
-  };
-
-  useEffect(() => {
+  function generateCloud(n) {
+    setOpen(true)
+    let count = n
     let final_data = [];
-    setCount(count);
     data.forEach((w) => {
       final_data.push([w.word, w.weight, w.click, w.color]);
     });
@@ -137,35 +154,29 @@ export default function Cloud({ prop }) {
       },
       rotationSteps: 2,
       rotateRatio: 0.4,
-      fontWeight: function (item, event, dimension) {
-        if (pop) {
-          return "bold";
-        }
-        return "normal";
-      },
       weightFactor: function (size, item) {
         let biggest = final_data[0][0].length;
         let max = maxWeight;
         if (biggest <= 7) {
-          if (size == max) {
-            return (Math.pow(size, 0.95) * (canvasWidth / 2)) / 1024;
+          if (size === max) {
+            return (Math.pow(size, 0.95) * (3 * (canvasWidth - 300) / 2)) / 1024;
           }
-          return (Math.pow(size, 0.75) * (canvasWidth / 2)) / 1024;
+          return (Math.pow(size, 0.75) * (3 * (canvasWidth - 300) / 2)) / 1024;
         } else if (biggest > 7 && biggest <= 10) {
-          if (size == max) {
-            return (Math.pow(size, 0.85) * (canvasWidth / 2)) / 1024;
+          if (size === max) {
+            return (Math.pow(size, 0.85) * (3 * (canvasWidth - 200) / 2)) / 1024;
           }
-          return (Math.pow(size, 0.75) * (canvasWidth / 2)) / 1024;
+          return (Math.pow(size, 0.75) * (3 * (canvasWidth - 300) / 2)) / 1024;
         } else if (biggest > 10 && biggest <= 13) {
-          if (size == max) {
-            return (Math.pow(size, 0.8) * (canvasWidth / 2)) / 1024;
+          if (size === max) {
+            return (Math.pow(size, 0.8) * (3 * (canvasWidth - 200) / 2)) / 1024;
           }
-          return (Math.pow(size, 0.75) * (canvasWidth / 2)) / 1024;
+          return (Math.pow(size, 0.75) * (3 * (canvasWidth - 300) / 2)) / 1024;
         } else if (biggest > 13) {
-          if (size == max) {
-            return (Math.pow(size, 0.7) * (canvasWidth / 2)) / 1024;
+          if (size === max) {
+            return (Math.pow(size, 0.75) * (4 * (canvasWidth - 200) / 2)) / 1024;
           }
-          return (Math.pow(size, 0.65) * (canvasWidth / 2)) / 1024;
+          return (Math.pow(size, 0.70) * (4 * (canvasWidth - 300) / 2)) / 1024;
         }
       },
       shrinkToFit: true,
@@ -177,111 +188,120 @@ export default function Cloud({ prop }) {
         popup(item, event, dimension);
       }
     });
+  }
+
+  useEffect(() => {
+
+    data.sort((a, b) => b["weight"] - a["weight"]);
+    let thumbnailArray = [[data[0].word, data[0].weight]]
+    setMaxWeight(data[0].weight);
+    Wordcloud(thumbnailCanvasRef.current, {
+      list: thumbnailArray,
+      shape: "rectangle",
+      shuffle: false,
+      rotateRatio: 0,
+      fontFamily: styles.fontFamily || "Raleway",
+      backgroundColor: styles.backgroundColor || "White",
+      color: (size, weight) => {
+        return "black";
+      },
+      rotationSteps: 0,
+      fontWeight: function (item, event, dimension) {
+        return "bold";
+      },
+      shrinkToFit: true,
+      minSize: 3,
+      drawOutOfBound: false,
+    });
   }, [maxWeight]);
   return (
     <div>
-      <div
-        style={{ display: "flex", justifyContent: "center", margin: "10px" }}
-      >
-        <div style={{ color: "black" }}>
-          Maximum : {max}
-          {"  "}
-          <input
-            type="number"
-            onChange={(e) => setNumber(e.target.value)}
-            style={{ margin: "10px auto" }}
-            placeholder="Enter Number of Words"
-          />
-          <Button
-            variant="contained"
-            onClick={handleSubmit}
-            style={{ marginLeft: "10px" }}
-          >
-            Submit
-          </Button>
-        </div>
-      </div>
-
-      <div
-        className="epic-word-cloud"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          margin: '10px',
-          cursor: "pointer"
-        }}
-        onMouseLeave={() => {
-          setPop(false);
-          setWord("")
-        }}
-        onClick={() => {
-          if (pop == true && word !== '') {
-            setPop(false)
-            setWord('')
-          }
-        }}
-
-      >
-        <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />
-
+      <div style={{ padding: '0px', marginLeft: '20px', display: 'flex', justifyContent: 'left' }}>
+        <canvas onClick={() => { open ? setOpen(false) : generateCloud(number) }} ref={thumbnailCanvasRef} width={thumbnailCanvasWidth} height={thumbnailCanvasHeight} />
         <div
-          hidden={!pop}
-          style={{
-            top: props.y,
-            left: props.x,
-            position: "absolute",
-            zIndex: 1,
-            backgroundColor: styles.hoverBg || "black",
-            color: "white",
-            cursor: "pointer",
-            color: "white",
-            minWidth: "200px",
-            minHeight: "100px",
-            maxWidth: "400px",
-            maxHeight: "400px",
-            padding: "5px",
-            lineHeight: "20px",
-            borderRadius: "15px",
-            fontSize: "20px",
-            textAlign: "center"
-          }}
+          style={{ margin: "10px" }}
         >
-          <div className="popHeading">
-            {word[0]}: {word[1]}
+          <div style={{ color: "black" }}>
+
+            <input
+              type="number"
+              onChange={(e) => setNumber(e.target.value)}
+              style={{ margin: "10px auto" }}
+              placeholder="Enter Number of Words"
+            />
+            {"  "}Maximum : {max}
           </div>
-          {word &&
-            word[2].map((link, idx) => (
-              <div key={idx} style={{ padding: "5px" }}>
-                <Link
-                  href={link.link}
-                  target="_blank"
-                  underline="hover"
-                  style={{ color: styles.linkColor || "blue" }}
-                >
-                  {link.label}
-                </Link>
-              </div>
-            ))}
         </div>
       </div>
-      {styles.caption && (
+      <Box hidden={!open} sx={style}>
+
         <div
-          style={{
-            display: "flex",
-            justifyContent: "center"
+          onMouseLeave={() => {
+            setPop(false);
+            setWord("")
+          }}
+          onClick={() => {
+            if (pop === true && word !== '') {
+              setPop(false)
+              setWord('')
+            }
           }}
         >
-          <h2
+          <canvas style={{ cursor: "pointer" }} ref={canvasRef} width={canvasWidth} height={canvasHeight} />
+          <div
+            hidden={!pop}
             style={{
-              fontFamily: styles.fontFamily || "Raleway",
-              fontSize: "50",
-              color: styles.captionColor || "blue"
+              top: props.y,
+              left: props.x,
+              position: "absolute",
+              zIndex: 1,
+              backgroundColor: styles.hoverBg || "black",
+              color: "white",
+              cursor: "pointer",
+              minWidth: "200px",
+              minHeight: "100px",
+              maxWidth: "400px",
+              maxHeight: "400px",
+              padding: "5px",
+              lineHeight: "20px",
+              borderRadius: "15px",
+              fontSize: "20px",
+              textAlign: "center"
             }}
           >
-            {styles.caption}
-          </h2>
+            <div className="popHeading">
+              {word[0]}: {word[1]}
+            </div>
+            {word &&
+              word[2].map((link, idx) => (
+                <div key={idx} style={{ padding: "5px" }}>
+                  <Link
+                    href={link.link}
+                    target="_blank"
+                    underline="hover"
+                    style={{ color: styles.linkColor || "blue" }}
+                  >
+                    {link.label}
+                  </Link>
+                </div>
+              ))}
+          </div>
         </div>
-      )}
+        {styles.caption && (
+          <div
+          >
+            <h2
+              style={{
+                fontFamily: styles.fontFamily || "Raleway",
+                fontSize: "50",
+                color: styles.captionColor || "blue"
+              }}
+            >
+              {styles.caption}
+            </h2>
+          </div>
+        )}
+      </Box>
     </div>
   );
 }
