d4adce91659dfe0ea76478eb87757123704cc9e8
unknown <roopeshb13@gmail.com>
2022-08-03 14:08:35 +0530

    modifications for popup to appear outside iframe

--- src/pages/iframe.js
+++ src/pages/iframe.js
@@ -1,5 +1,4 @@
-import { useRef, useState } from "react";
-import { Link, Box, } from '@mui/material'
+import { useState } from "react";
 
 
 
@@ -9,7 +8,6 @@ function Iframe() {
     const file = require("../../public/" + filename + ".json")
     const styles = file.style
     const source = `http://localhost:3000?input=${filename}&thumbnail=false&popup=false`
-    const componentRef = useRef(null);
     const displayWord = styles.popup.displayWord !== undefined ? styles.popup.displayWord : false;
     const displayCount = styles.popup.displayCount !== undefined ? styles.popup.displayCount : false;
 
@@ -32,54 +30,46 @@ function Iframe() {
 
     return (
         <div style={{ paddingLeft: "50px", display: 'flex' }}>
-
             <iframe title="cloud" height='700px' width='900px' src={source}></iframe>
-
-            {pop && <Box id="popuphover"
-                ref={componentRef}
-                hidden={!pop} sx={{
+            {pop &&
+                <div id="popuphover" hidden={!pop} style={{
+                    position: "absolute",
                     top: props.y,
                     left: props.x,
-                    position: "absolute",
-                    minWidth: styles.popup.width || "250px",
                     minHeight: styles.popup.minHeight || "100px",
+                    minWidth: styles.popup.width || "260px",
                     zIndex: 1,
                     fontSize: styles.popup.fontSize || "22px",
                     fontFamily: styles.fontFamily || "Raleway",
-                    backgroundColor: styles.popup.backgroundColor || "black",
+                    backgroundColor: styles.popup.backgroundColor || "white",
                     color: styles.popup.fontColor || "black",
                     transform: 'translate(7%,10%)',
-                    boxShadow: 24,
+                    boxShadow: "2px 2px 20px 2px #888888",
                     transitionTimingFunction: "ease",
                     transition: "height 0.3s",
-                    py: 1.5,
+                    paddingTop: "15px",
+                    paddingBottom: "15px",
                 }}
-                onMouseLeave={() => { popup() }}
-            >
-                {(displayWord || false) && <div id='popup' style={{ paddingTop: '8px', paddingLeft: '28px', paddingBottom: '8px', paddingRight: '30px', display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
-                    {element.charAt(0).toUpperCase() + element.slice(1, element.length)}
-                </div>}
-                {(displayCount || false) && <div id='popup' style={{ "padding": "8px 30px 8px 30px" }}>
-                    Count: {word[1]}
-                </div>}
-                {(displayWord || false) && <div style={{ padding: '5px', borderBottom: '1px solid grey' }}></div>}
-                {(displayWord || false) && <div style={{ padding: '5px' }}></div>}
-                {word &&
-                    word[2].map((link, idx) => (
-                        <div id='popup' key={idx} >
-                            <Link
-                                href={link.link}
-                                target="_blank"
-                                underline="none"
-                                style={{ color: styles.popup.linkColor || "blue", }}
-                            >
-                                <div id='popup' key={idx} style={{ "padding": "8px 30px 8px 30px" }}>
-                                    {link.label.charAt(0).toUpperCase() + link.label.slice(1, link.label.length)}
+                    onMouseLeave={() => { popup() }}>
+                    {(displayWord || false) && <div id='popup' style={{ paddingTop: '8px', paddingLeft: '28px', paddingBottom: '8px', paddingRight: '30px', display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
+                        {element.charAt(0).toUpperCase() + element.slice(1, element.length)}
+                    </div>}
+                    {(displayCount || false) && <div id='popup' style={{ "padding": "8px 30px 8px 30px" }}>
+                        Count: {word[1]}
+                    </div>}
+                    {(displayWord || false) && <div style={{ padding: '5px', borderBottom: '1px solid grey' }}></div>}
+                    {(displayWord || false) && <div style={{ padding: '5px' }}></div>}
+                    {word &&
+                        word[2].map((link, idx) => (
+                            <div id='popup' key={idx} >
+                                <div id='popup' key={idx} style={{ "padding": "8px 30px 8px 30px" }} >
+                                    <a style={{ color: 'black', fontFamily: styles.fontFamily || "Raleway", textDecoration: 'none' }} href={link.link} target="_blank" rel="noreferrer">
+                                        {link.label.charAt(0).toUpperCase() + link.label.slice(1, link.label.length)}
+                                    </a>
                                 </div>
-                            </Link>
-                        </div>
-                    ))}
-            </Box>}
+                            </div>
+                        ))}
+                </div>}
         </div >
     );
 }
